2f7461251783fba78bae6130eadf73965651645d
Celine Kim <celinekim@Celines-MacBook-Pro.local>
2020-11-13 22:29:55 -0800

    account code page

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function numPadInput(inputId, buttonText) {
+  let input = document.getElementById(inputId);
+  let length = input.value.length;
+
+  if (buttonText == "Delete") {
+    input.value = input.value.substring(0, length-1);
+  }
+  else if (buttonText == "Enter") {
+    input.value = "";
+  }
+  else if (buttonText == "Return") {
+    
+  }
+  else {
+    if (length >= 4) {
+      return;
+    }
+    input.value = input.value + buttonText;
+  }
+}
