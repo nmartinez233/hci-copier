477e46a67b486c018cab209163f762c2618f40cc
Nathaniel Martinez <nmartinez23@uchicago.edu>
2020-11-15 20:30:28 -0600

    Uploading sides + seperator page

--- js/main.js
+++ js/main.js
@@ -20,3 +20,7 @@ function numPadInput(inputId, buttonText) {
   }
 }
 
+function switch_div(show) {  
+  document.getElementById("show_"+show).style.display = "block";
+  document.getElementById("show_"+((show==1)?2:1)).style.display = "none";
+}
